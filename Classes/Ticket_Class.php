9b55d0d589f4dcf6da07774dc6bcc1fcc1d478e6
Agon.Hoxhaj <agon.hoxhaj@student.uni-pr.edu>
2026-03-27 17:11:41 +0100

    user class add ons

--- Classes/Ticket_Class.php
+++ Classes/Ticket_Class.php
@@ -4,15 +4,18 @@ require 'Classes/Event_Class.php';
 class Ticket {
     public $event;
     private $user;
+    public $price;
 
-    function set_details_from_values($event_name, $event_type, $location, $date, $user) {
+    function set_details_from_values($event_name, $event_type, $location, $date, $user, $price) {
         $this->event = new Event($event_name, $event_type, $location, $date);
         $this->user = $user;
+        $this->price = $price;
     }
 
-    function set_details_from_event($event, $user) {
+    function set_details_from_event($event, $user, $price) {
         $this->event = $event;
         $this->user = $user;
+        $this->price = $price;
     }
 
     function get_details() {
--- Classes/User_class.php
+++ Classes/User_class.php
@@ -3,27 +3,32 @@ require "Ticket_Class.php";
 class User{
 
     private $id;
-    private $name;
+    private $username;
     private $email; 
     private $passwordHash; 
     private $user_type; 
     private array $tickets = [];
     
     
-    public function __construct($id, $name, $email, $password, $user_type){
+    function __construct($id, $username, $email, $password, $user_type){
         $this->id = $id; 
-        $this->name = $name;
+        $this->username = $username;
         $this->email =$email;
         $this->passwordHash = password_hash($password, PASSWORD_DEFAULT);
         $this->user_type = $user_type;
     }
 
-    public function add_ticket(Ticket $ticket){
+    function add_ticket(Ticket $ticket){
         $this->tickets[] = $ticket;
     }
 
-    public function get_tickets(){
+    function get_tickets(){
         return $this->tickets;
+    }
+
+    function get_UserDetails(){
+       echo "Username: " . $this->username . "<br> Email: " . $this->email .".<br> ";
+       echo "Tickets: " . $this->get_tickets();
     }
 }
 
