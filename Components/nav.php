520f7c2e83d7bf51b2156d3579d40a12f912c649
Agon.Hoxhaj <agon.hoxhaj@student.uni-pr.edu>
2026-04-04 13:57:13 +0200

    session username in landing

--- Components/nav.php
+++ Components/nav.php
@@ -24,6 +24,7 @@
         <a class="nav-link" href="#">Disabled</a>
       </li>
     </ul>
+    <a href="" style="padding-right: 10px; color: black;"><?php echo $_SESSION['username'];?></a>
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">Search</button>
