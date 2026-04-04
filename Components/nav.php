cbfac627c4420f3e6b7cdc8a3e71f595e76c1ecb
Art-Gashi <5453arti@gmail.com>
2026-04-04 16:47:06 +0200

    Navbar color change

--- Components/nav.php
+++ Components/nav.php
@@ -6,7 +6,7 @@
 </head>
 <body style="background-color : black" >
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -24,10 +24,10 @@
         <a class="nav-link" href="../Pages/events.php">Events</a>
       </li>
     </ul>
-    <a href="../Pages/profile.php" style="padding-right: 10px; color: black;"><?php echo $_SESSION['username'];?></a>
+    <a href="../Pages/profile.php" style="padding-right: 10px; color: white;"><?php echo $_SESSION['username'];?></a>
     <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">Search</button>
+      <input class="form-control mr-sm-2 bg-dark text-white border-secondary" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
     </form>
   </div>
 </nav>
