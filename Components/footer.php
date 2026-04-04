695c88c0df1766a0ebf486d9fc50efd8aba78110
Agon.Hoxhaj <agon.hoxhaj@student.uni-pr.edu>
2026-04-04 16:22:43 +0200

    fixed home page and profile redirection

--- Components/footer.php
+++ Components/footer.php
@@ -7,10 +7,7 @@
   <div class="container-fluid">
     <div class="p-4 pb-0">
       <p class="d-flex justify-content-center align-items-center">
-        <span class="me-3">Register for free!</span>
-        <button style="margin-left: 7px;" type="button" class="btn btn-outline-dark btn-rounded">
-          Sign up!
-        </button>
+       
       </p>
     </div>
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
